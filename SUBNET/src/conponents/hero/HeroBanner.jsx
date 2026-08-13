989dd790ecf2dc11894f05d7466497dee1cd9fb4
jayace-7 <jayacehob@gmail.com>
2026-08-13 01:35:33 +0100

    hero fix 2

--- SUBNET/src/conponents/hero/HeroBanner.jsx
+++ SUBNET/src/conponents/hero/HeroBanner.jsx
@@ -1,7 +1,14 @@
+import heroImage from "../../assets/hero/dune-part-two-hero.jpg";
+import heroTrailer from "../../assets/hero/dune-part-two-preview.mp4";
+
+
 function HeroBanner() {
   return (
-      <section>
-        <p />
+      <section className="hero-banner">
+        <img src={heroImage} alt="Dune Part Two" className="hero-image" />
+        <div className="hero-trailer">
+          <video src={heroTrailer} controls muted loop />
+        </div>
       </section>
   );
 }
